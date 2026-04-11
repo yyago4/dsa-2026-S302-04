8e6a1ccd3d6e035c4c8355620c640599d1143953
ihorsa777 <ihor.sablin01@estudiant.upf.edu>
2026-04-11 12:46:31 +0000

    actuarilar main(dos ultimos puntos de lab 2)

--- src/main.c
+++ src/main.c
@@ -54,6 +54,14 @@ void normalize_street(char* str){   //amb aquesta funció ajustarem el text sego
   }
 }
 
+
+int count_matches(const char*s1, const char *s2){//implementamos una funcion que compara los caracteres de una str con la otra en las miismas posiciones
+int matches =0;
+for(int i=0; s1[i]!='\0'&& s2[i]!='\0';i++){
+  if(s1[i]==s2[i]){matches++;}
+}
+return matches;
+}
 //void createaleak() {
   //char *foo = malloc(20 * sizeof(char));
   //printf("Allocated leaking string: %s", foo);
@@ -103,7 +111,13 @@ int main() {
         normalize_street(search_norm);
 
         int found = 0;  //varibale per saber si hem trobat la casa
+        int street_exists =0; // variable para saber si es el numero que no existe dentro de una calle real, o porque la calle no existe
         house* current = list;  //comencem a busacr desde el principi de la llista
+
+        
+
+
+
 
         while (current!=NULL){  //mirem casa per casa fins arribar al final de la llista
           char current_street_norm[MAX_STR];  //fem espai per poder normalitzar el carrer de la llista
@@ -111,15 +125,69 @@ int main() {
           normalize_street(current_street_norm);  //normaliztem el carrer de la llista
 
 
-          if (strcmp(current_street_norm, search_norm)== 0 && current->num == address_number){  //comprovem si el carrer normalitzat i el numero coincideixen.
+          if (strcmp(current_street_norm, search_norm)== 0){  //comprovem si el carrer normalitzat .
+            street_exists=1;
+            if(current->num == address_number){//si el numero tambe coincideix
             printf("Coordinates: Lat %f, Lon %f\n", current->lat, current->lon);  //printejem el resultat
             found = 1;  //marquem que l0hem trobat
             break;  // sortim del bucle ara queja l'hem trobat
+            }
           }
           current = current->next;  //pasem a la seguent casa
         }
         if (!found){  //avisem si hem mirat totes i no hem trobat res
-          printf("Address not found in this map.\n");
+          if(street_exists){ // caso en que la calle es correcta pero el numero no
+            printf("Street found but number %d is invalid. Please choose a valid number:\n", address_number);
+            current=list;//posicionamos al principio de la lista de todas las casas
+            while(current!=NULL){
+              char csn[MAX_STR];
+              strcpy(csn,current->street);//copiamos la calle en variable aux
+              normalize_street(csn);
+              if (strcmp(csn,search_norm)==0){//miramos si scn(calle actual normalizada) y serach_norm(calle buscada normalizada) son las mismas
+                printf("-%d\n",current->num);//imprimimos la dirrecion para el usuario
+
+              }
+              current=current->next;//recorremos toda la lista(lista de todas las casas del mapa) para ver las coincidencias de las calles
+            }
+          }
+          else{//caso que la calle no se sepa
+            printf("Street not known. Did you mean one of these?\n");
+            current=list;
+            char suggestions[5][MAX_STR];//hacemos matriz para guardar las 5 sugerencias
+            int sug_count=0; // contador de sugerencias
+            int max_matches[5] = {0, 0, 0, 0, 0};//para guardar los matches que tiene cada calle (para poner el top y no sol primeros 5)
+            while(current!=NULL && sug_count<5){
+              char csn[MAX_STR];
+              strcpy(csn,current->street);
+              normalize_street(csn);
+              int matches=count_matches(search_norm,csn); //num de parecidos
+              if(matches>= strlen(search_norm)/2 && matches > 2){ // condiciones para entrar en la lista: parecidos almenos la mitad de la longitud de la str y >2
+                int already_in=-1;//auxiliar para ver si la calle ya esta en la lista
+                for(int i=0;i<sug_count;i++){//la primera iteracion se lo salta ya que es la primera calle y si o si no esta en la lista
+                  if(strcmp(suggestions[i],current->street==0)){ already_in=i;}
+                }
+                if(already_in=-1){//si la calle es nueva probamos de meterla en el top
+                  for(int i=0;i<5;i++){
+                    if(matches>max_matches[i]){//si la calle actual es mejor que la del top en posicion i
+                      for(int j=4;j>i;j--){//movemos todos los de abajo una posicion abajo
+                        max_matches[j]=max_matches[j-1];
+                        strcpy(suggestions[j],suggestions[j-1]);
+                      }
+                      max_matches[i]=matches;//metemos el nuevo
+                      strcpy(suggestions[i],current->street);
+                      if(sug_count<5){sug_count++;}
+                      break;
+                  }
+                }
+
+              }
+
+            }
+            current=current->next;
+          }
+          for(int i=0;i<sug_count;i++){
+            printf("%d: %s (Coincidencias: %d)\n",i+1,suggestions[i],max_matches[i]);//imprimimos el top para el usuario
+          }
         }
     }
 
